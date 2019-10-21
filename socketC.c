53b77a9e9c47b05c7f1c0d4aaa42859ffc7b9d4f
qpal147147 <43807991+qpal147147@users.noreply.github.com>
2019-10-21 18:54:08 +0800

    Client

--- socketC.c
+++ socketC.c
@@ -8,18 +8,35 @@
 #include<netdb.h>
 
 
-int main(){
+int main(int argc,char *argv[]){
     int servfd,portNo = 6666,clifd;
     int clientLen;
     char sendBuffer[256];
     struct sockaddr_in serv_addr;
+    struct hostent *serv;
     int n;
+
+    //print ipPort & refence
+    for(int i = 0;i<argc;i++){
+        printf("arg:%d argv:%s\n",argc,argv[i]);
+    }
+
+    //set ipPort
+    if(argc>=2){
+        portNo = atoi(argv[2]);
+    }
 
     servfd = socket(AF_INET,SOCK_STREAM,0);
     if(servfd >=0){
         serv_addr.sin_family = AF_INET;
         serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-        serv_addr.sin_port = htons(portNo);
+        
+	if(argc >=2){
+	    serv = gethostbyname(argv[1]);
+	    bcopy((char *)serv->h_addr,(char *)&serv_addr.sin_addr.s_addr,
+            serv->h_length);
+	}
+	serv_addr.sin_port = htons(portNo);
 
         if(connect(servfd,(struct sockaddr*)&serv_addr,sizeof(serv_addr)) >=0){
             while(1){
@@ -28,6 +45,10 @@ int main(){
                 fgets(sendBuffer,256,stdin);
                 n = write(servfd,sendBuffer,strlen(sendBuffer));
                 printf("Send Messenge(%d):%s\n",n,sendBuffer);
+                
+		        if((n =read(servfd,sendBuffer,256)) > 0){
+  		            printf("Recv Messenge(%d):%s\n",n,sendBuffer);
+		        }
             }
         }
     }
