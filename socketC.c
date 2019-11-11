52426422a2b209d8046de187b2b07fcd3b65e84f
qpal147147 <43807991+qpal147147@users.noreply.github.com>
2019-11-11 18:27:07 +0800

    Update socketC.c
    Client

--- socketC.c
+++ socketC.c
@@ -6,12 +6,13 @@
 #include<sys/socket.h>
 #include<netinet/in.h>
 #include<netdb.h>
+#include<netinet/tcp.h>
 
 
 int main(int argc,char *argv[]){
     int servfd,portNo = 6666,clifd;
     int clientLen;
-    char sendBuffer[256];
+    char sendBuffer[256],recBuffer[256];
     struct sockaddr_in serv_addr;
     struct hostent *serv;
     int n;
@@ -27,15 +28,18 @@ int main(int argc,char *argv[]){
     }
 
     servfd = socket(AF_INET,SOCK_STREAM,0);
+    int flag = 1;
+    setsockopt(servfd,IPPROTO_TCP,TCP_NODELAY,(char *)&flag,sizeof(flag));
+	
     if(servfd >=0){
         serv_addr.sin_family = AF_INET;
-        serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+        serv_addr.sin_addr.s_addr = inet_addr("10.0.0.1");
         
 	if(argc >=2){
 	    serv = gethostbyname(argv[1]);
-	    bcopy((char *)serv->h_addr,(char *)&serv_addr.sin_addr.s_addr,
-            serv->h_length);
+	    bcopy((char *)serv->h_addr,(char *)&serv_addr.sin_addr.s_addr,serv->h_length);
 	}
+		
 	serv_addr.sin_port = htons(portNo);
 
         if(connect(servfd,(struct sockaddr*)&serv_addr,sizeof(serv_addr)) >=0){
@@ -43,15 +47,41 @@ int main(int argc,char *argv[]){
                 printf("Enter Messenge:");
                 bzero(sendBuffer,256);
                 fgets(sendBuffer,256,stdin);
-                n = write(servfd,sendBuffer,strlen(sendBuffer));
-                printf("Send Messenge(%d):%s\n",n,sendBuffer);
                 
-		        if((n =read(servfd,sendBuffer,256)) > 0){
-  		            printf("Recv Messenge(%d):%s\n",n,sendBuffer);
-		        }
+                if(strncmp(sendBuffer,"/FILE",5)==0){
+                    printf("Send file command start\n");
+                    n = write(servfd,sendBuffer,strlen(sendBuffer));
+                	
+                    for(int i = 0;i<100000000;i++);
+                	
+                    FILE *fp = fopen("hello","r");
+                    int rByte = 0;
+                    char fileBuffer[256] = {0};
+                	
+                    while((rByte = fread(fileBuffer,1,sizeof(fileBuffer),fp))>0){
+                	n = write(servfd,fileBuffer,strlen(fileBuffer));
+                	bzero(fileBuffer,sizeof(fileBuffer));
+                    }
+                	
+                    for(int i = 0;i<100000000;i++);
+                	
+                    char EOFBuffer[] = {EOF};
+                    write(servfd,EOFBuffer,sizeof(EOFBuffer));
+                    fclose(fp);
+                    printf("Send file command end\n");
+                }
+                else{
+		    n = write(servfd,sendBuffer,strlen(sendBuffer));
+		    printf("Send Messenge(%d):%s\n",n,sendBuffer);
+		}
+		        
+		bzero(recBuffer,sizeof(recBuffer));
+		if((n =read(servfd,sendBuffer,256)) > 0){
+	  	    printf("Recv Messenge(%d):%s\n",n,sendBuffer);
+		}
             }
         }
     }
-
+    close(servfd);
     return 0;
 }
