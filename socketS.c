9bdf735fe853571149c0ba3a2265ccb25afedf9f
qpal147147 <43807991+qpal147147@users.noreply.github.com>
2019-11-11 18:23:34 +0800

    Update socketS.c
    Server

--- socketS.c
+++ socketS.c
@@ -9,11 +9,11 @@
 int main(){
     int servfd,portNo=6666,clifd;
     int clientLen;
-    char recBuffer[256], sendBuff[]={"Thank You!"};
+    char recBuffer[256]={0}, sendBuff[]={"Thank You!"};
     struct sockaddr_in serv_addr,cli_addr;
     int n;
-
     servfd = socket(AF_INET,SOCK_STREAM,0);
+
     if(servfd >= 0){
         serv_addr.sin_family = AF_INET;
         serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
@@ -21,14 +21,43 @@ int main(){
 
         if(bind(servfd,(struct sockaddr*)&serv_addr,sizeof(serv_addr)) >= 0){
             listen(servfd,1);
-            clifd = accept(servfd,(struct sockaddr*)&serv_addr,&serv_addr);
+	    clientLen = sizeof(cli_addr);
+            clifd = accept(servfd,(struct sockaddr*)&cli_addr,&clientLen);
+            
             while(1){
-                bzero(recBuffer,256);
-                if((n =read(clifd,recBuffer,256)) > 0){
+                bzero(recBuffer,sizeof(recBuffer));
+                
+                if((n =read(clifd,recBuffer,sizeof(recBuffer))) > 0){
   		    printf("Messenge:%s\n",recBuffer);
-		    n = write(clifd,sendBuff,strlen(sendBuff));
+  		    		
+		    if(strncmp(recBuffer,"/FILE",5)==0){
+		 	rintf("recv file ommand start\n");
+			FILE *fp = fopen("recv.txt","w");
+			int rByte = 0;
+						
+			while((rByte = read(clifd,recBuffer,sizeof(recBuffer)))>0){
+			    char EOFBuffer[] = {EOF};
+							
+			    if(strncmp(recBuffer,EOFBuffer,1)==0){
+				break;
+			    }
+							
+			    printf("file:%s\n",recBuffer);
+			    fwrite(recBuffer,strlen(recBuffer),1,fp);
+			    bzero(recBuffer,sizeof(recBuffer));
+			}
+			fclose(fp);
+						
+			char fileEnd[] = {"File End"};
+			n = write(clifd,fileEnd,strlen(fileEnd));
+			printf("recv file command end\n");
+		
+		    }
+		    else{
+		        n = write(clifd,sendBuff,strlen(sendBuff));
+		    }
 		}
-            }
+           }
         }
     }
 }
