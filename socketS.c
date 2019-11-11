4b5a8b4cad513a98f12abf8e77b789e7c56c5d21
qpal147147 <43807991+qpal147147@users.noreply.github.com>
2019-11-11 23:41:19 +0800

    Update socketS.c

--- socketS.c
+++ socketS.c
@@ -9,7 +9,7 @@
 int main(){
     int servfd,portNo=6666,clifd;
     int clientLen;
-    char recBuffer[256]={0}, sendBuff[]={"Thank You!"};
+    char recBuffer[256]={0}, sendBuff[]={"Thank You!\nYou will get the same response"};
     struct sockaddr_in serv_addr,cli_addr;
     int n;
     servfd = socket(AF_INET,SOCK_STREAM,0);
@@ -21,43 +21,47 @@ int main(){
 
         if(bind(servfd,(struct sockaddr*)&serv_addr,sizeof(serv_addr)) >= 0){
             listen(servfd,1);
-	    clientLen = sizeof(cli_addr);
+			clientLen = sizeof(cli_addr);
             clifd = accept(servfd,(struct sockaddr*)&cli_addr,&clientLen);
             
             while(1){
                 bzero(recBuffer,sizeof(recBuffer));
                 
                 if((n =read(clifd,recBuffer,sizeof(recBuffer))) > 0){
-  		    printf("Messenge:%s\n",recBuffer);
+				printf("Messenge:%s\n",recBuffer);
   		    		
-		    if(strncmp(recBuffer,"/FILE",5)==0){
-		 	rintf("recv file ommand start\n");
-			FILE *fp = fopen("recv.txt","w");
-			int rByte = 0;
+					if(strstr(recBuffer,"/FILE [")!=NULL){
+						char *fileName = strchr(recBuffer,'[') + 1;
+						strtok(fileName,"]");
 						
-			while((rByte = read(clifd,recBuffer,sizeof(recBuffer)))>0){
-			    char EOFBuffer[] = {EOF};
+						printf("recv file ommand start\n");
+						FILE *fp = fopen(strcat(fileName,"Copy"),"w");
+						int rByte = 0;
 							
-			    if(strncmp(recBuffer,EOFBuffer,1)==0){
-				break;
-			    }
+						while((rByte = read(clifd,recBuffer,sizeof(recBuffer)))>0){
+							char EOFBuffer[] = {EOF};
+										
+							if(strncmp(recBuffer,EOFBuffer,1)==0){
+							break;
+							}
+									
+							printf("file:%s\n",recBuffer);
+							fwrite(recBuffer,strlen(recBuffer),1,fp);
+							bzero(recBuffer,sizeof(recBuffer));
+						}
+						fclose(fp);
 							
-			    printf("file:%s\n",recBuffer);
-			    fwrite(recBuffer,strlen(recBuffer),1,fp);
-			    bzero(recBuffer,sizeof(recBuffer));
+						char fileEnd[] = {"File End"};
+						n = write(clifd,fileEnd,strlen(fileEnd));
+						printf("recv file command end\n");
+				
+					}
+					else{
+						n = write(clifd,sendBuff,strlen(sendBuff));
+					}
+				}
 			}
-			fclose(fp);
-						
-			char fileEnd[] = {"File End"};
-			n = write(clifd,fileEnd,strlen(fileEnd));
-			printf("recv file command end\n");
-		
-		    }
-		    else{
-		        n = write(clifd,sendBuff,strlen(sendBuff));
-		    }
 		}
-           }
-        }
     }
 }
+
