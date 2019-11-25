55bd75223c6b85a63f4d95bd6430256bd7841728
qpal147147 <43807991+qpal147147@users.noreply.github.com>
2019-11-25 19:07:45 +0800

    Update socketS.c

--- socketS.c
+++ socketS.c
@@ -5,14 +5,25 @@
 #include<sys/types.h>
 #include<sys/socket.h>
 #include<netinet/in.h>
+typedef struct{
+	int id;
+	char name[20];
+}user;
 
 int main(){
     int servfd,portNo=7777,clifd;
     int clientLen;
+	user users[20];
     char recBuffer[256]={0}, sendBuffer[]={"Thank You!"};
     struct sockaddr_in serv_addr,cli_addr;
     int n;
     
+	//initialization user name
+	for(int index = 0;index<20;index++){
+		users[index].id = 0;
+		snprintf(users[index].name,sizeof(users[index].name),"User %d",index-3);	
+	}
+	
     //chat
     int fdmax = -1,retval = 0;
     fd_set tmpSet,master;
@@ -53,15 +64,41 @@ int main(){
             					if(clifd != -1){
             						FD_SET(clifd,&master);
             						if(clifd > fdmax) fdmax = clifd;
-            						printf("NO[%d] is online!\n",clifd);
+            						printf("NO[%d] => %s online!\n",clifd,users[clifd].name);
             						/* and then broadcast this news */
+            						
+            						fd_set broSet = master;
+            						for(int j = 0;j<=fdmax;j++){
+            							if(FD_ISSET(j,&broSet)){
+            								if(j!=servfd && j != clifd){
+            									char writeBuff[256] = {0};
+            									snprintf(writeBuff,sizeof(writeBuff), " %s online!",
+            									users[clifd].name);
+            									write(j,writeBuff,strlen(writeBuff));
+            									
+            								}
+            							}
+            						}
             					}
             				}
             				else{ //client say something or disconnection
             					if((n=read(i,recBuffer,sizeof(recBuffer))) <= 0){
             						if(n <= 0){
-            							printf("NO[%d] leave!\n",i);
+            							printf("NO[%d] => %s leave!\n",i,users[i].name);
             							/* and then broadcast this news */
+            							
+            							fd_set broSet = master;
+		        						for(int j = 0;j<=fdmax;j++){
+		        							if(FD_ISSET(j,&broSet)){
+		        								if(j!=servfd && j != i){
+		        									char writeBuff[256] = {0};
+		        									snprintf(writeBuff,sizeof(writeBuff), " %s leave!",
+		        									users[i].name);
+		        									write(j,writeBuff,strlen(writeBuff));
+		        									
+		        								}
+		        							}
+		        						}
             						}
             						
             						close(i);
@@ -73,7 +110,8 @@ int main(){
             							if(FD_ISSET(j,&broSet)){
             								if(j!=servfd && j != i){
             									char writeBuff[256] = {0};
-            									snprintf(writeBuff,sizeof(writeBuff), " No[%d]: %s", i, recBuffer);
+            									snprintf(writeBuff,sizeof(writeBuff), " No[%d]: %s",
+            									i, recBuffer);
             									write(j,writeBuff,strlen(writeBuff));
             									
             								}
