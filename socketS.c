b74ccb8deb3c48a6b6d77a070e80c9849de2276b
qpal147147 <43807991+qpal147147@users.noreply.github.com>
2019-12-09 19:43:09 +0800

    Update socketS.c

--- socketS.c
+++ socketS.c
@@ -35,7 +35,7 @@ int main(){
 
 	if(servfd >= 0){
 	serv_addr.sin_family = AF_INET;
-	serv_addr.sin_addr.s_addr = inet_addr("10.0.0.1");
+	serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");//Multiplayer 10.0.0.1
 	serv_addr.sin_port = htons(portNo);
 
 		if(bind(servfd,(struct sockaddr*)&serv_addr,sizeof(serv_addr)) >= 0){
